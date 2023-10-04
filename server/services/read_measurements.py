ed28cfdf103ec678a1e5ea71dfa95a3b207e088d
Ismael Lima Rocha <ismaellimarocha23@gmail.com>
2023-10-04 11:20:23 -0300

    fix: power generation mean total value not being divided by 15

--- server/services/read_measurements.py
+++ server/services/read_measurements.py
@@ -14,26 +14,27 @@ def read_measurements():
     current_time = time.time()
     time_elapsed_since_last_measurement = current_time - last_time
 
-    power_generation_total_sum_15_minutes = 0.0
-
-    if time_elapsed_since_last_measurement < seconds_between_measurements:
-        pass
-    else:
+    if time_elapsed_since_last_measurement >= seconds_between_measurements:
         current_row_number += math.floor(
             time_elapsed_since_last_measurement / seconds_between_measurements
         )
+
         last_time = current_time
 
-        for i in range(15):
-            row_number = current_row_number * 15 + i
+    power_generation_total_sum_15_minutes = 0.0
 
-            power_generation_data_1_minute = read_csv(
-                "data/power_generation.csv", row_number
-            )
+    for i in range(15):
+        row_number = current_row_number * 15 + i
 
-            power_generation_total_sum_15_minutes += float(
-                power_generation_data_1_minute[1]
-            )
+        power_generation_data_1_minute = read_csv(
+            "data/power_generation.csv", row_number
+        )
+
+        power_generation_total_sum_15_minutes += float(
+            power_generation_data_1_minute[1]
+        )
+
+    power_generation_mean_value = power_generation_total_sum_15_minutes / 15
 
     consumption_data_15_minutes = read_csv(
         "data/power_consumption.csv", current_row_number
@@ -44,5 +45,5 @@ def read_measurements():
         "consumption_measurements": [
             float(value) for value in consumption_data_15_minutes[1:]
         ],
-        "generation_measurements": [round(power_generation_total_sum_15_minutes, 2)],
+        "generation_measurements": [round(power_generation_mean_value, 2)],
     }
